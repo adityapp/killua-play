cd725b31a756804b84eceae8610afb5c7bed8510
adityapp <aditpp97@gmail.com>
2024-06-18 21:12:09 +0700

    change wa button size and message

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <main className="flex flex-col">
       <Image src="/images/banner.png" width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }}/>
-      <Link href="https://wa.me/6281517044263">
-        <Image className="fixed bottom-0 right-0 m-10" src="/images/whatsapp.png" width={64} height={64}/>
+      <Link href="https://wa.me/6281517044263?text=Hallo%20saya%20mau%20sewa%20dong">
+        <Image className="fixed bottom-0 right-0 m-4 md:m-10" src="/images/whatsapp.png" width={60} height={60}/>
       </Link>
     </main>
   );
